c159a802b53f5b0182316d9c5747673c7cbcb134
Anonymushadow <83378179+Anonymushadow@users.noreply.github.com>
2022-11-04 17:10:46 -0300

    intento 26

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res)=>{
-    res.render("index.ejs", {
-        pagina: "inicio"
-    });
-});
+
 
 router.get("/inicio", (req, res)=>{
     res.render("inicio.ejs", {titulo: "Sobre Nosotros"});

f2b4995855d93b11aede5588767f9388a9bc2375
Anonymushadow <83378179+Anonymushadow@users.noreply.github.com>
2022-11-05 14:39:58 -0300

    intento 43

--- Routes/routes.js
+++ Routes/routes.js
@@ -4,23 +4,23 @@ const router = express.Router();
 
 
 router.get("/inicio", (req, res)=>{
-    res.render("inicio.ejs", {titulo: "Sobre Nosotros"});
+    res.render("partials/inicio.ejs", {titulo: "Sobre Nosotros"});
 });
 
 router.get("/nosotros", (req, res)=>{
-    res.render("empresa.ejs", {titulo: "Sobre Nosotros"});
+    res.render("partials/empresa.ejs", {titulo: "Sobre Nosotros"});
 });
 
 router.get("/servicios", (req, res)=>{
-    res.render("servicios.ejs", {titulo: "Nuestros Servicios"});
+    res.render("partials/servicios.ejs", {titulo: "Nuestros Servicios"});
 });
 
 router.get("/preguntasFrecuentes", (req, res)=>{
-    res.render("preguntas-frecuentes.ejs", {titulo: "Preguntas Frecuentes"});
+    res.render("partials/preguntas-frecuentes.ejs", {titulo: "Preguntas Frecuentes"});
 });
 
 router.get("/contactenos", (req, res)=>{
-    res.render("contact-us.ejs", {titulo: "Contactenos"});
+    res.render("partials/contact-us.ejs", {titulo: "Contactenos"});
 });
 
 module.exports = router;
